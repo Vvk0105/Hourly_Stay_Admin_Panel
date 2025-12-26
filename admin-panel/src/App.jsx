79a452e1da8cc5434d99c28fc338de9129ad1ea4
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-12-26 16:13:18 +0530

    add protechedroute and save store from refresh

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { initAuth } from "./utils/initAuth";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    initAuth(dispatch);
+  }, []);
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- admin-panel/src/store/authSlice.js
+++ admin-panel/src/store/authSlice.js
@@ -3,14 +3,17 @@ import { createSlice } from '@reduxjs/toolkit';
 const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        user: null
+        user: null,
+        initialized: false,
     },
     reducers: {
         setUser(state, action) {
             state.user = action.payload
+            state.initialized = true
         },
         logout(state) {
             state.user = null;
+            state.initialized = true
             localStorage.clear()
         }
     }
